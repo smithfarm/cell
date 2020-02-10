790216b3dc25f8f50f3037851483841c30f9a2f2
Nathan Cutler <ncutler@suse.com>
2020-02-10 22:47:43 +0100

    0.223
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/CELL.pm
+++ lib/App/CELL.pm
@@ -54,11 +54,11 @@ App::CELL - Configuration, Error-handling, Localization, and Logging
 
 =head1 VERSION
 
-Version 0.222
+Version 0.223
 
 =cut
 
-our $VERSION = '0.222';
+our $VERSION = '0.223';
 
 
 
--- lib/App/CELL/Guide.pm
+++ lib/App/CELL/Guide.pm
@@ -46,11 +46,11 @@ App::CELL::Guide - Introduction to App::CELL (POD-only module)
 
 =head1 VERSION
 
-Version 0.222
+Version 0.223
 
 =cut
 
-our $VERSION = '0.222';
+our $VERSION = '0.223';
 
 
 
